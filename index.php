7f8f21383a03176d0b22ec1222804e8c931df66a
Hải Long <hailong2004ptcnn@gmail.com>
2025-03-19 11:32:46 +0700

    feat: initialize project structure with database connection and basic layout

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
+<?php
+  session_start();
+
+  include("lib/sql_connect.php");
+?>
+
 <!DOCTYPE html>
 <html>
 
+<head>
+  <title>Cổng quản lý sinh viên</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="css/main.css">
+</head>
 <body>
-  <?php
-  echo ("<h2>Hello World</h2>");
-  ?>
+  <h1>Trang web quản lý sinh viên của trường ABC</h1>
+
+  <script src="js/jquery.js"></script>
+  <script src="js/index.js"></script>
 </body>
 
 </html>
---
+++ lib/sql_connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "127.0.0.1";
+$username = "user";
+$password = "p@ssw0rd";
+$database = "challenge5a_longlh14";
+
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->connect_error) {
+  exit("Error connecting to DB: ". $conn->connect_error);
+}
+
+?>
